2504ea8dd0f0847ca892a8cb4c7d66a5db31a652
Lescure Guillaume <guillaume.lescure@parrot.com>
2015-03-11 17:59:38 +0100

    Use StackTrace instead of custom trace.

--- src/gov/base/logs/logs.go
+++ src/gov/base/logs/logs.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
-	"strconv"
+	"runtime/debug"
 )
 
 const (
@@ -44,16 +43,8 @@ func Information(verbose uint, msg ...interface{}) {
 	if this.verbose >= verbose {
 		if this.color == true {
 			log.Println(colors.StringColored(colors.Bold, colors.Green, colors.Black, information) + " " + fmt.Sprint(msg...))
-
-			if this.verbose == 3 {
-				moreColoredInformation()
-			}
 		} else {
 			log.Println(information, fmt.Sprint(msg...))
-
-			if this.verbose == 3 {
-				moreInformation()
-			}
 		}
 	}
 }
@@ -62,16 +53,8 @@ func Warning(msg ...interface{}) {
 	if this.verbose > 0 {
 		if this.color == true {
 			fmt.Println(colors.StringColored(colors.Bold, colors.Orange, colors.Black, warning) + " " + fmt.Sprint(msg...))
-
-			if this.verbose == 3 {
-				moreColoredInformation()
-			}
 		} else {
 			log.Println(warning + " " + fmt.Sprint(msg...))
-
-			if this.verbose == 3 {
-				moreInformation()
-			}
 		}
 	}
 }
@@ -79,26 +62,9 @@ func Warning(msg ...interface{}) {
 func Error(msg ...interface{}) {
 	if this.color == true {
 		log.Println(colors.StringColored(colors.Bold, colors.Red, colors.Black, error) + " " + fmt.Sprint(msg...))
-
-		if this.verbose == 3 {
-			moreColoredInformation()
-		}
 	} else {
 		log.Println(error + " " + fmt.Sprint(msg...))
-
-		if this.verbose == 3 {
-			moreInformation()
-		}
 	}
+	debug.PrintStack()
 	os.Exit(1)
-}
-
-func moreColoredInformation() {
-	function, file, line, _ := runtime.Caller(2)
-	fmt.Println("(at the line " + colors.StringColored(colors.Underline, colors.White, colors.Black, strconv.FormatInt((int64)(line), 10)) + " of the function '" + colors.StringColored(colors.Underline, colors.White, colors.Black, runtime.FuncForPC(function).Name()) + "' in the file '" + colors.StringColored(colors.Underline, colors.White, colors.Black, file) + "')")
-}
-
-func moreInformation() {
-	function, file, line, _ := runtime.Caller(2)
-	fmt.Println("(at the line " + strconv.FormatInt((int64)(line), 10) + " of the function '" + runtime.FuncForPC(function).Name() + "' in the file '" + file + "')")
 }
