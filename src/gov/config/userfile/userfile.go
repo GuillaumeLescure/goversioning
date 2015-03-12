12c9fc83dc89201c48b0cc8537f2c22154c12040
Lescure Guillaume <guillaume.lescure@parrot.com>
2015-03-12 09:40:42 +0100

    Create and load userConfFile.

--- src/gov/config/userfile/userfile.go
+++ src/gov/config/userfile/userfile.go
@@ -1,41 +1,96 @@
 package userfile
 
 import (
+	"gov/config/version"
 	"gov/base/logs"
 	"encoding/xml"
 	"os"
+	"os/user"
 )
 
 type (
-	userFile struct {
-		name    string
-		mail    string
-		editor  string
-		verbose uint
-		color   bool
+	self struct {
+		firstName   string
+		lastName    string
+		mail        string
+		editor      string
+		verbose     uint
+		color       bool
+	}
+
+	dataFromFile struct {
+		XMLName     xml.Name    `xml:"GoVersioning"`
+		Type        string      `xml:"type,attr"`
+		Version     string      `xml:"version,attr"`
+		FirstName   string      `xml:"user>firstName"`
+		LastName    string      `xml:"user>lastName"`
+		Mail        string      `xml:"user>mail"`
+		Editor      string      `xml:"preferences>editor"`
+		Verbose     uint        `xml:"preferences>verbose"`
+		Color       bool        `xml:"preferences>color"`
 	}
 )
 
 var (
-	this userFile
+	this    self
+
+	DefaultPath string
 )
 
-func ReadAndSetFile(path string) error {
+func init() {
+	currentUser, _ := user.Current()
+	DefaultPath = currentUser.HomeDir + "/.gov.conf"
+}
+
+func ReadOrCreateFile(path string) {
+	if _, err := os.Stat(path); err == nil {
+		LoadFile(path)
+	} else {
+		WriteDefaultFile(path)
+	}
+}
+
+func LoadFile(path string) {
 	file, err := os.Open(path)
 	if err != nil {
-		return err
+		logs.Error(err);
 	}
 	defer file.Close()
 
-	err = xml.NewDecoder(file).Decode(&this)
+	data := dataFromFile{}
+	err = xml.NewDecoder(file).Decode(&data)
 	if err != nil {
 		logs.Error(err)
 	}
-	return nil
+
+	logs.Warning(data)
 }
 
-func Name() string {
-	return this.name
+func WriteDefaultFile(path string) {
+	file, err := os.Create(path)
+	if err != nil {
+		logs.Error(err);
+	}
+	defer file.Close()
+
+	data := dataFromFile{XMLName:xml.Name{" ", "GoVersioning"}, Type:"userConfFile", Version:version.Version(), FirstName:"???", LastName:"???", Mail:"???", Editor:"vi", Verbose:0, Color:false}
+	xmlToWrite, err := xml.MarshalIndent(data, "", "    ")
+	if err != nil {
+		logs.Error(err)
+	}
+
+	_, err = file.Write(xmlToWrite)
+	if err != nil {
+		logs.Error(err);
+	}
+}
+
+func FirstName() string {
+	return this.firstName
+}
+
+func LastName() string {
+	return this.lastName
 }
 
 func Mail() string {
